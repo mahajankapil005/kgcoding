dcfe6564b8bac8d31be5c0cf86c524cda10deeee
mahajankapil005 <130069883+mahajankapil005@users.noreply.github.com>
2024-01-05 17:09:15 +0000

    2ND DONE

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,32 +1,21 @@
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   // "Dal","Green vegetables","Roti","Milk","Rice"
-
   let foodItems = ["Dal", "Green vegetables", "Roti", "Milk", "Rice"];
-
-  if (foodItems.length === 0) {
-    return <h3>I am still hungry</h3>
-  }
   return (
     <>
       <h1>Healthy Food Items</h1>
+      {foodItems.length === 0 ? <h3>i am still hungry.</h3> : null}
       <ul className="list-group">
-        {foodItems.map((item)=>{
-          <li className="list-group-item">{item}</li>
-        })}
-        
-        
+        {foodItems.map(item => 
+        <li key={item} className="list-group-item">{item}</li>
+        )}
       </ul>
-      {/* <ul className="list-group">
-        {foodItems.map((item)=>{
-          <li key={item} className="list-group-item">{item}</li>
-        })}
-         
-      </ul> */}
-    </>
-  );
+   </>
+  )  
 }
 
 export default App;
